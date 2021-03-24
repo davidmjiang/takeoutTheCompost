75f73edca8b19b1d89f52b18086b58793717b09c
David Jiang <davidjgrey@gmail.com>
2021-03-24 16:27:06 -0700

    move env variables to env file

---
+++ config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const config = {};
+
+config.host = process.env.HOST;
+config.authKey = process.env.AUTH_KEY;
+config.databaseId = "CompostApp";
+config.containerId = "Reviews";
+
+if (config.host.includes("https://localhost:")) {
+  console.log("Local environment detected");
+  console.log("WARNING: Disabled checking of self-signed certs. Do not have this code in production.");
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  console.log(`Go to http://localhost:${process.env.PORT || '3000'} to try the sample.`);
+}
+
+config.yelpKey = process.env.YELP_KEY;
+
+module.exports = config;
