62eb1cee980370ab2479dbf0f2d254ce45c3121a
David Jiang <davidjgrey@gmail.com>
2021-03-25 09:39:50 -0700

    get review by id api

--- routes/reviews.js
+++ routes/reviews.js
@@ -17,6 +17,21 @@ const ReviewDao = require("../models/reviewDao");
      res.send(items);
    }
 
+   async getReview(req, res) {
+     const yelpId = req.params.id;
+     const querySpec = {
+       query: "SELECT * FROM root r WHERE r.yelpId=@yelpId",
+       parameters: [
+         {
+           name: "@yelpId",
+           value: yelpId
+         }
+       ]
+     };
+     const review = await this.reviewDao.find(querySpec);
+     res.send(review);
+   }
+
    async addReview(req, res) {
      const item = req.body;
 
--- server.js
+++ server.js
@@ -44,6 +44,7 @@ reviewDao.init(err => {
 });
 
 app.get("/reviews", (req, res, next) => reviewList.getReviews(req, res).catch(next));
+app.get("/review/:id", (req, res, next) => reviewList.getReview(req, res).catch(next));
 app.get("/searchResults", (req, res, next) => yelpApi.searchBusinesses(req, res));
 app.post("/review", (req, res, next) => reviewList.addReview(req, res).catch(next));
 
